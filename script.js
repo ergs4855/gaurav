c90a2175805a996c0d345078621d653660b9c60d
ergs4855 <ergs4855@gmail.com>
2025-06-05 16:35:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function showMessage() {
-  alert("Tum.....Tumhare....BSDK");
+  alert("Jai Hind");
 }
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
